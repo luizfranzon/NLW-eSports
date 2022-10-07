df96bacf49019b776d7fd65b02be7869f7d75647
Luiz <luizfranzon05@hotmail.com>
2022-10-06 23:28:08 -0300

    sim kkkkkkkk

--- App/mobile/App.tsx
+++ App/mobile/App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "react-native";
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter'
 
+import { Routes } from "./src/routes";
 import { Home } from './src/screens/Home'
 import { Loading } from "./src/components/Loading";
 import { Background } from "./src/components/Background";
@@ -16,7 +17,7 @@ export default function App() {
     return (
         <Background>
             <StatusBar barStyle={"light-content"} backgroundColor="transparent" translucent/>
-            {fontsLoaded ? <Home/> : <Loading/>}
+            {fontsLoaded ? <Routes/> : <Loading/>}
             
         </Background>
     );
